bae8803dc527fff0ec59d83862eb867da91bee70
Negasus <negasus@gmail.com>
2024-03-25 11:21:57 +0300

    refactoring

--- cmd/goval/rule_in_test.go
+++ cmd/goval/rule_in_test.go
@@ -1,88 +1,56 @@
 package main
 
-import (
-	"reflect"
-	"testing"
-)
+import "testing"
 
-func Test_parseInput(t *testing.T) {
+func Test_isRuleVar(t *testing.T) {
 	type args struct {
-		input string
+		rule string
 	}
 	tests := []struct {
-		name string
-		args args
-		want []string
+		name  string
+		args  args
+		want  string
+		want1 bool
 	}{
 		{
-			name: "empty",
-			args: args{input: ""},
-			want: []string{""},
+			name:  "empty",
+			args:  args{rule: ""},
+			want:  "",
+			want1: false,
 		},
 		{
-			name: "simple1",
-			args: args{input: "foo,bar"},
-			want: []string{"foo", "bar"},
+			name:  "small size",
+			args:  args{rule: "{}"},
+			want:  "",
+			want1: false,
 		},
 		{
-			name: "spaces",
-			args: args{input: " foo , bar , ,"},
-			want: []string{" foo ", " bar ", " ", ""},
+			name:  "no brace 1",
+			args:  args{rule: "a{a}"},
+			want:  "",
+			want1: false,
 		},
 		{
-			name: "quotes",
-			args: args{input: "foo,'bar','baz\\''"},
-			want: []string{"foo", "bar", "baz'"},
+			name:  "no brace 2",
+			args:  args{rule: "{a}a"},
+			want:  "",
+			want1: false,
 		},
 		{
-			name: "comma in quotes",
-			args: args{input: "foo,'ba,r','baz\\''"},
-			want: []string{"foo", "ba,r", "baz'"},
+			name:  "ok",
+			args:  args{rule: "{foo}"},
+			want:  "foo",
+			want1: true,
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := parseStringInput(tt.args.input); !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("parseInput() = %v, want %v", got, tt.want)
+			got, got1 := isRuleVar(tt.args.rule)
+			if got != tt.want {
+				t.Errorf("isRuleVar() got = %v, want %v", got, tt.want)
 			}
-		})
-	}
-}
-
-func Test_makeString(t *testing.T) {
-	type args struct {
-		slice []string
-	}
-	tests := []struct {
-		name string
-		args args
-		want string
-	}{
-		{
-			name: "empty",
-			args: args{slice: []string{}},
-			want: "{}",
-		},
-		{
-			name: "one",
-			args: args{slice: []string{"foo"}},
-			want: `{"foo"}`,
-		},
-		{
-			name: "many",
-			args: args{slice: []string{"foo", "bar", " baz "}},
-			want: `{"foo", "bar", " baz "}`,
-		},
-		{
-			name: "escape",
-			args: args{slice: []string{`fo " bar`}},
-			want: `{"fo \" bar"}`,
-		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			if got := makeStringFromStrings(tt.args.slice); got != tt.want {
-				t.Errorf("makeString() = %v, want %v", got, tt.want)
+			if got1 != tt.want1 {
+				t.Errorf("isRuleVar() got1 = %v, want %v", got1, tt.want1)
 			}
 		})
 	}
